1879a85de9da28b3326975a677d1d1a1d11406d7
Arya Roy <aryaroy121951@gmail.com>
2026-09-08 12:12:55 +0530

    Add number guessing game in main.c
    Implement a number guessing game with user feedback.

--- main.c
+++ main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main(){
+    srand(time(NULL)); // Changes the randomly generated number each time someone runs the program.
+    int n=rand()%100+1; // Generates a randomly generated number from 1-100.
+    int guess,i=0; 
+    char text[] = "Welcome to Guess the Number!";
+    for (int j = 0; text[j] != '\0'; j++) //This particular loop is for adding gravity to the appearance of "Welcome" screen!
+    {
+        printf("%c", text[j]);
+        fflush(stdout);
+        Sleep(30);
+    }
+    printf("\n");
+    Sleep(1000); // Further gravity before user starts guessing the number 
+        while (1)
+        {
+            printf("Guess The Number : ");
+            scanf("%d", &guess);
+            if (guess == n)
+            {
+                i++;
+                printf("You have guessed the number! It took you a total of %d try(s)!\n", i);
+                break;
+            }
+            else
+            {
+                i++;
+                printf("%d was not the number! Trial number : %d!\n", guess, i);
+                continue;
+            }
+        }
+        return 0;
+    }
