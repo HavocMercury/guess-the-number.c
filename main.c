a591d3443063867aa75e658b75c9921f6f455476
Arya Roy <aryaroy121951@gmail.com>
2026-09-15 00:07:43 +0530

    Refactor number guessing game input handling, further improved input validation and "higher or lower" statements

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
-int main(){
-    srand(time(NULL)); // Changes the randomly generated number each time someone runs the program.
-    int n=rand()%100+1; // Generates a randomly generated number from 1-100.
-    int guess,i=0; 
+int main()
+{
+    srand(time(NULL));        // Changes the randomly generated number each time someone runs the program.
+    int n = rand() % 100 + 1; // Generates a randomly generated number from 1-100.
+    int guess, i = 0;
+    int check = 0, lengthchecker = 0;
+    char input[100];
+    int valid = 1;
+
     char text[] = "Welcome to Guess the Number!";
     for (int j = 0; text[j] != '\0'; j++) // This particular loop is for adding gravity to the appearance of "Welcome" screen!
     {
@@ -15,13 +21,31 @@ int main(){
         Sleep(30);
     }
     printf("\n");
-    Sleep(1000); // Further gravity before user starts guessing the number 
-        while (1)
-        {
-            printf("Guess The Number : ");
-            scanf("%d", &guess); // User inputs their guess
+    Sleep(1000); // Further gravity before user starts guessing the number
 
-          if(guess >=1 && guess <=100 ){ // Checks whether or not, number entered by user is in the valid range 
+    while (1)
+    {
+        printf("Guess The Number : ");
+        scanf("%99s", input);
+        valid=1; 
+        // Check every character
+        for (int i = 0; input[i] != '\0'; i++)
+        {
+            if (!isdigit((unsigned char)input[i]))
+            {
+                valid=0;
+                break;
+            }
+        }
+
+        if (!valid)
+        {
+            printf("Invalid input, you have to choose a number! \n");
+            continue;
+        }
+        guess = atoi(input); // Converts the array to integer for further checking whether its in range or not, and if its the guess
+        if (guess >= 1 && guess <= 100) // Checks whether or not, number entered by user is in the valid range
+        {
             if (guess == n)
             {
                 i++;
@@ -32,13 +56,23 @@ int main(){
             {
                 i++;
                 printf("%d was not the number! Trial number : %d!\n", guess, i);
+                if (guess > n) // this portion is so help the user to some extent when it comes to guessing the number
+                { 
+                    printf("Go lower!\n");
+                }
+                else if (guess < n)
+                {
+                    printf("Go higher!\n");
+                }
                 continue;
             }
         }
-          else{
-            printf("%d is not in the given range (1-100)!\n",guess);
-            continue;
-          }
+        else
+        {
+                printf("%d is not in the given range (1-100)!\n", guess);
+                continue;
         }
-        return 0;
     }
+     return 0;
+}
+
